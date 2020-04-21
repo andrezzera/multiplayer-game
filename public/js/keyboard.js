708a0256eb25ffed95da5dffed95cdd49046fd49
Andrezzera <andrezzera@outlook.com>
2020-04-21 12:38:02 -0300

    Create the keyboard listener

---
+++ public/js/keyboard.js
@@ -0,0 +1,38 @@
+function keyBorardListener(document) {
+
+    const state = {
+        playerId: null,
+        observers: []
+    }
+
+    function setPlayerId(playerId) {
+        state.playerId = playerId
+    }
+
+    function subscribe(observerFunction) {
+        state.observers.push(observerFunction)
+    }
+
+    function notifyAll(command) {
+        for(const observerFunction in state.observers) {            
+            observerFunction(command)
+        }
+    }
+
+    document.addEventListener('keydown', handleKeyDown)
+
+    function handleKeyDown(event) {
+        const keyPressed = event.key
+        const command = {
+            command: 'move-player',
+            playerId: state.playerId,
+            keyPressed
+        }
+        notifyAll(command)
+    }
+
+    return {
+        subscribe,
+        setPlayerId
+    }
+}
